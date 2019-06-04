6ed7491f922b0c9cec26e6647658dfd2582a18f8
Neil <neil.limaye108@gmail.com>
2019-06-03 23:04:24 -0500

    Updates

---
+++ precache-manifest.50f84b5f48accb61917ca0a6d48a221c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8467e2db6aa7fa6e841f8cb981299f3c",
+    "url": "/bettertemptool/index.html"
+  },
+  {
+    "revision": "9287edf396cdc8d95a2e",
+    "url": "/bettertemptool/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/bettertemptool/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "9287edf396cdc8d95a2e",
+    "url": "/bettertemptool/static/js/main.4f363841.chunk.js"
+  },
+  {
+    "revision": "6241f91b33ffc4eb0a2d",
+    "url": "/bettertemptool/static/js/runtime~main.44ff9878.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bettertemptool/static/media/logo.5d5d9eef.svg"
+  }
+]);
